0f1b6bce7b7bcf828bace2512cc2fdc2033d390a
Gabriel Alves dos Santos <galves@mundolivre.digital>
2025-08-12 14:43:40 -0300

    Alterações FInais - 12-08

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
 import time
 
 from desktop import desk_abrir, desk_form
-from script import web_login, web_valor, web_form
+from web import web_login, web_valor, web_form
 from api import conectarapi
 
 def main():
+    loop = 0
     web_login()
-    valor2 = web_valor()
-    app =  desk_abrir(valor2)
-    campo_valor = desk_form(app)
-    web_form(campo_valor)
-    dados_funcionario = conectarapi(valor2)
-    app.kill()
 
-    telefoneapi = dados_funcionario.get('phoneNumber')
-    inicioapi = dados_funcionario.get('startDate')
-    idapi = dados_funcionario.get('employeeID')
+    while loop < 10:
+        valor2 = web_valor()
+        app =  desk_abrir(valor2)
+        campo_valor = desk_form(app)
+        web_form(campo_valor)
+        dados_funcionario = conectarapi(valor2)
+        app.kill()
+
+        telefoneapi = dados_funcionario.get('phoneNumber')
+        inicioapi = dados_funcionario.get('startDate')
+        idapi = dados_funcionario.get('employeeID')
 
 
 
-    nome, sobrenome, telefone, email, cidade, estado, postal, cargo, setor, inicio, gestor, enviar = web_form(app)
+        nome, sobrenome, telefone, email, cidade, estado, postal, cargo, setor, inicio, gestor, enviar = web_form(app)
 
-    time.sleep(1)
-    nome.send_keys(campo_valor[0])
-    sobrenome.send_keys(campo_valor[1])
-    telefone.send_keys(telefoneapi)
-    email.send_keys(campo_valor[2])
-    cidade.send_keys(campo_valor[3])
-    postal.send_keys(campo_valor[4])
-    estado.send_keys(campo_valor[5])
-    setor.send_keys(campo_valor[7])
-    cargo.send_keys(campo_valor[8])
-    inicio.send_keys(inicioapi)
-    gestor.send_keys(campo_valor[6])
-    time.sleep(1)
+        time.sleep(1)
+        nome.send_keys(campo_valor[0])
+        sobrenome.send_keys(campo_valor[1])
+        telefone.send_keys(telefoneapi)
+        email.send_keys(campo_valor[2])
+        cidade.send_keys(campo_valor[3])
+        postal.send_keys(campo_valor[4])
+        estado.send_keys(campo_valor[5])
+        setor.send_keys(campo_valor[7])
+        cargo.send_keys(campo_valor[8])
+        inicio.send_keys(inicioapi)
+        gestor.send_keys(campo_valor[6])
+        time.sleep(1)
 
-    enviar.click()
+        enviar.click()
+
+        loop = loop + 1
 
     input('Clique algo para continuar')
 
